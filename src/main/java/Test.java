ad5c7731aed839f825e03d35c961499387508bb4
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-01-21 19:44:26 +0600

    Delete unused statements

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,6 @@
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class Test {
