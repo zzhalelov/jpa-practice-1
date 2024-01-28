f5bc2158e23f7fd5e17d1f576db718e99cf46d76
Zhastilek Zhalelov <zzhalelov@MacBook-Air-Zastilek.local>
2024-01-28 20:47:55 +0600

    Set annotations in Category.java class

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Category {
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
 }
