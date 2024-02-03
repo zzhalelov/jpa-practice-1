641e364764a56e5ee6250789c41d82cc1f344cfa
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-03 18:39:44 +0600

    Abstract class EntityDao.java

---
+++ src/main/java/dao/EntityDao.java
@@ -0,0 +1,64 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public abstract class EntityDao<T> {
+    protected static EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
+    protected static EntityManager manager = factory.createEntityManager();
+
+    private final Class<T> type;
+
+    public EntityDao(Class<T> type) {
+        this.type = type;
+    }
+
+    public T findById(int id) {
+        return manager.find(type, id);
+    }
+
+    public List<T> findAll() {
+        return manager.createQuery("SELECT t FROM " + type.getSimpleName() + " t", type).getResultList();
+    }
+
+    public void update(T entity) {
+        try {
+            manager.getTransaction().begin();
+
+            manager.merge(entity);
+
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            manager.getTransaction().rollback();
+        }
+    }
+
+    public void delete(T entity) {
+        try {
+            manager.getTransaction().begin();
+
+            manager.remove(entity);
+
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            manager.getTransaction().rollback();
+        }
+    }
+
+    public void create(T entity) {
+        try {
+            manager.getTransaction().begin();
+
+            manager.persist(entity);
+
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            manager.getTransaction().rollback();
+        }
+    }
+}
