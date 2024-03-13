60e08bd863a4f89b3fb3dcb89edade2a53294a0c
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-13 13:36:20 +0500

    fix: add ManyToMany & ManyToOne relationships in Order.java

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -6,6 +6,7 @@ import model.Status;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,4 +23,18 @@ public class Order {
 
     @Column(name = "date_of_order")
     private LocalDateTime dateOfOrder;
+
+    @ManyToMany
+    @JoinTable(
+            name = "orders_products",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<Product> productList;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItemList;
 }
