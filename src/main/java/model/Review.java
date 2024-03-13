af0e4a40e7539820a45d5afa2b414484cadc77fe
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-13 13:47:52 +0500

    feat: create class Review, add lombok annotations

---
+++ src/main/java/model/Review.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+}
